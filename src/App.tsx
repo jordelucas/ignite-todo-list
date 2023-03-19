bcfbbc56e4b4bc766420739092ebe2aef3aa4b84
jordelucas <jordevalucas@hotmail.com>
2023-03-19 20:19:52 -0300

    adjust App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Header } from './components/Header'
+import { TaskList } from './components/TaskList'
+import { NewTaskInput } from './components/NewTaskInput'
+import { TaskListInfo } from './components/TaskListInfo'
+
 import './globals.css'
 
+import styles from './App.module.css'
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div>
+      <Header />
+
+      <main className={styles.wrapper}>
+        <NewTaskInput /> 
+
+        <TaskListInfo />
+
+        <TaskList />
+      </main>
+    </div>
+  )
 }
